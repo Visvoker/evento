e3ad1a2630295025cd87b253dec8c37accd5173c
visvoker <s2089533@gmail.com>
2025-05-09 16:20:26 +0800

    Page params for dynamic H1

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import H1 from "@/components/h1";
+import { usePathname } from "next/navigation";
 import React from "react";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function EventsPage({ params }: EventsPageProps) {
+  const pathName = usePathname();
+  const city = params.city;
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[100vh]">
-      <H1>Event in Austin</H1>
+      {city === "all" ? (
+        <H1>All Events</H1>
+      ) : (
+        <H1>Event in {city.charAt(0).toUpperCase() + city.slice(1)}</H1>
+      )}
     </main>
   );
 }
