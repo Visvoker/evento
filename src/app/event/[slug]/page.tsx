71d13f1f20673f7b2d9a9c29d4bac60bd4bada8c
visvoker <s2089533@gmail.com>
2025-05-12 17:56:58 +0800

    Finish event page intro section

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/components/h1";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -15,24 +16,45 @@ export default async function EventPage({ params }: EventPageProps) {
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20 ">
         <Image
           src={event.imageUrl}
-          className="object-cover blur-3xl"
+          className="object-cover z-0 blur-3xl"
           alt="Event background image"
           fill
           quality={50}
           sizes="(max-width:1280px) 100vw, 1280px"
+          priority
         />
 
-        <div>
+        <div className="z-1 flex flex-col relative gap-6 lg:gap-16 lg:flex-row">
           <Image
-            className="z-1 relative"
             src={event.imageUrl}
+            className="rounded-xl border-2 border-white/50 object-cover"
             alt={event.name}
             width={300}
             height={201}
           />
+
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <H1>{event.name}</H1>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span>{event.organizerName}</span>
+            </p>
+
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] sm:w-full py-2 rounded-md border-white/10 border-2 hover:scale-105 active:scale-[1.02] transition focus:scale-105">
+              Get tickets
+            </button>
+          </div>
         </div>
       </section>
 
