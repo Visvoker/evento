e149ba933bfbdf69d11e523eef52b2eea6d79ef9
visvoker <s2089533@gmail.com>
2025-05-13 20:52:08 +0800

    Data fetching Utilities

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,23 +1,12 @@
-import { EventoEvent } from "@/lib/type";
-import React from "react";
 import EventCard from "./events-card";
-import { sleep } from "@/lib/utils";
+import { getEvents } from "@/lib/utils";
 
 type EventsListProps = {
   city: string;
 };
 
 export default async function EventsList({ city }: EventsListProps) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
-    {
-      next: {
-        revalidate: 300,
-      },
-    }
-  );
-
-  const events: EventoEvent[] = await response.json();
+  const events = await getEvents(city);
 
   return (
     <section className="flex flex-wrap max-w-[1100px] gap-10 justify-center px-[20px]">
