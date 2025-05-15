4fec877a516d78135bb9e6e9767ce90dc693ee60
visvoker <s2089533@gmail.com>
2025-05-15 09:59:17 +0800

    NotFound() function in NextJS for event page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 import prisma from "./db";
+import { notFound } from "next/navigation";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -36,5 +37,9 @@ export async function getEvent(slug: string) {
       slug: slug,
     },
   });
+
+  if (!event) {
+    return notFound();
+  }
   return event;
 }
